3eda1a1b9ff96a272d6e3245c1dd955f1cf273c0
Adrian Roman <aroman_ro@yahoo.com>
2020-01-11 12:21:34 +0200

    Avoid double deletion if options is copied

--- KKR/Options.h
+++ KKR/Options.h
@@ -12,6 +12,29 @@ class Options
 public:
 	Options();
 
+	// avoid double deletion of m_fileconfig at destruction if copied
+	Options(const Options& other)
+		:
+		nrThreads(other.nrThreads),
+		nrPoints(other.nrPoints),
+		pathNo(other.pathNo),
+		paths(other.paths),
+		m_fileconfig(nullptr)
+	{
+	}
+
+	Options& operator=(const Options& other)
+	{
+		nrThreads = other.nrThreads;
+		nrPoints = other.nrPoints;
+		pathNo = other.pathNo;
+		paths = other.paths;
+		m_fileconfig = nullptr;
+
+		return *this;
+	}
+
+
 	void Load();
 	void Save();
 
