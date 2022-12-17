095e7fa7f4d29df80a95f88460835dcf7a4d796c
Adrian Roman <aroman_ro@yahoo.com>
2022-12-17 10:20:19 +0200

    Minor change to factorial

--- KKR/Coefficients.h
+++ KKR/Coefficients.h
@@ -20,8 +20,7 @@ namespace KKR
 	public:
 		static double Factorial(long long int n)
 		{
-			// using precalculated (at compile time) values
-			if (n < 0) return 1;
+			if (n <= 1) return 1;
 			else if (n < 21) return static_cast<double>(factorialsTable[n]);
 
 			double val = static_cast<double>(factorialsTable[20]);
